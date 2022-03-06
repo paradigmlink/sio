97d0efc1b8daa886814845b3821886ec226921ac
Stewart Mackenzie <setori88@gmail.com>
2022-03-06 18:00:36 +0200

    Problem: receive statement should allow for flexible non-determinism or full determinism
    Solution: remove the  and instead introduce a sliding window of non-determinism with each case of the receive statement: i.e. include a list of who is able to access

--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -12,8 +12,8 @@ pub struct SioParser;
 fn main() {
     let unparsed_file =
 r#"
-mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/list {
-  name2 :: () -> {I64: Hi} {
+mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/list 100 {
+  name2 :: () -> {i64: Hi} {
     hi = {1:2}
   }
 }
@@ -41,11 +41,14 @@ mod parse_tests {
     fn data_variant_test() {
         let input =
         r#"
-       mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+       mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             data DataType = Constructor
-            data DataType = Constructor([Int], [Int])
-            data DataType = Sheep({name: Bool, naked: Bool})
-            data DataType<A,B,C> = Constructor(Option<T>, Result<String, I64>)
+            data DataType = Constructor([i64], [i64])
+            data DataType =
+                | Constructor([i64], [i64])
+                | Constructor([i64], [i64])
+            data DataType = Sheep({name: bool, naked: bool})
+            data DataType<A,B,C> = Constructor(Option<T>, Result<string, i64>)
             data A =
                 | C
                 | D
@@ -61,23 +64,23 @@ mod parse_tests {
                 | None
                 | Some(T)
             data E =
-                | sketch MyArray ([|3; Option<Result<I64, String>>|], I64)
-                | stable MyList([Option<Result<I64, String>>], I64)
-                | summon MyTuple((Option<Result<I64, String>>, I64))
-                | sunset MySet(<Option<Result<I64, String>>>, I64)
+                | sketch MyArray ([|3; Option<Result<i64, string>>|], i64)
+                | stable MyList([Option<Result<i64, string>>], i64)
+                | summon MyTuple((Option<Result<i64, string>>, i64))
+                | sunset MySet(<Option<Result<i64, string>>>, i64)
                 | seeyou MyRecord({
-                    an_atom: [|2; I64|],
-                    Bool: [I64],
-                    I64:  (I64),
-                    String:<I64>,
-                    Char: {I64:Option<Result<I64, String>>}
+                    an_atom: [|2; i64|],
+                    bool: [i64],
+                    i64:  (i64),
+                    string:<i64>,
+                    char: {i64:Option<Result<i64, string>>}
                   })
             data F = MyRecord({
-                an_atom: [|2; I64|],
-                Bool: [I64],
-                I64:  (I64),
-                String:<I64>,
-                Char: {I64:Option<Result<I64, String>>}
+                an_atom: [|2; i64|],
+                bool: [i64],
+                i64:  (i64),
+                string:<i64>,
+                char: {i64:Option<Result<i64, string>>}
             })
             a: A
             b: B
@@ -86,11 +89,11 @@ mod parse_tests {
             e_tuple: E
             e_set: E
             e_record: E
-            f: F<I64>
-            k_1: K<I64, Bool>
-            k_2: K<I64, Bool>
-            p_1: P<I64>
-            p_2: P<Option<I65>>
+            f: F<i64>
+            k_1: K<i64, bool>
+            k_2: K<i64, bool>
+            p_1: P<i64>
+            p_2: P<Option<i64>>
             name0 :: () -> Simple {
                 skip
                 a = A
@@ -140,7 +143,7 @@ mod parse_tests {
     fn pattern_match_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             name0 :: () -> Simple {
                 match [|3|] {
                     | [first|second|tail] => { skip }
@@ -152,10 +155,10 @@ mod parse_tests {
                     | 'h' => { skip }
                     | _ => { skip }
                     | "hello" => { skip }
-                    | val: Int => { skip }
-                    | (val: Int, str: String) => { skip }
-                    | Rectangle(hi: Hi) => { skip }
-                    | Rectangle(width: Int, height: Int) => { skip }
+                    | val: i64 => { skip }
+                    | (val: i64, str: string) => { skip }
+                    | Rectangle(hi) => { skip }
+                    | Rectangle(width, height) => { skip }
                 }
             }
         }
@@ -184,9 +187,9 @@ mod parse_tests {
     fn capitalized_record_key_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
-            data Export = Export({ export_procedure: Char, ExportType: Char })
-            summon name :: () -> String {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
+            data Export = Export({ export_procedure: char, ExportType: char })
+            summon name :: () -> string {
                 hi = Export({export_procedure, ExportType})
             }
         }
@@ -215,7 +218,7 @@ mod parse_tests {
     fn var_to_var_binding_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             data Rec = Rec({ name: (), name: (A)->A })
             name0 :: () -> Simple {
                 a = Type
@@ -233,7 +236,7 @@ mod parse_tests {
                 k = () -> Hi { skip }
                 k = (hi: Hi) -> Hi { skip }
                 k = (hi: Hi, hi: Hi) -> Hi { skip }
-                k = (hi: Hi, hi: Hi<I64>) -> Result<Option<I64>, String> { skip }
+                k = (hi: Hi, hi: Hi<i64>) -> Result<Option<i64>, string> { skip }
                 k = (hi: [|3; Hi|], hi: [Hi]) -> <Hi> {
                     skip
                     hi=[|3,2,1|]
@@ -286,7 +289,7 @@ mod parse_tests {
     fn print_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             name0 :: () -> Simple {
                 hi = {1:2}
                 print("...")
@@ -332,24 +335,24 @@ mod parse_tests {
     fn return_value_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
-            summon name :: (str: String) -> String { skip x="string" }
-            summon name :: () -> String { "string" }
-            summon name :: () -> String { 32 }
-            summon name :: () -> String { 32+32 }
-            summon name :: () -> String { 32*32*32 }
-            summon name :: () -> String { 32.42 }
-            summon name :: () -> String { true }
-            summon name :: () -> String { false }
-            summon name :: () -> String { 'c' }
-            summon name :: () -> String { (32,42) }
-            summon name :: () -> String { <32,42> }
-            summon name :: () -> String { {thirty_two: 32, fourty_two: 42} }
-            summon name :: () -> String { [32, 42] }
-            summon name :: () -> String { [|32, 42|] }
-            summon name :: () -> String { Constructor }
-            summon name :: () -> String { Constructor([32]) }
-            summon name :: () -> String { Constructor([32], [32]) }
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
+            summon name :: (str: string) -> string { skip x="string" }
+            summon name :: () -> string { "string" }
+            summon name :: () -> string { 32 }
+            summon name :: () -> string { 32+32 }
+            summon name :: () -> string { 32*32*32 }
+            summon name :: () -> string { 32.42 }
+            summon name :: () -> string { true }
+            summon name :: () -> string { false }
+            summon name :: () -> string { 'c' }
+            summon name :: () -> string { (32,42) }
+            summon name :: () -> string { <32,42> }
+            summon name :: () -> string { {thirty_two: 32, fourty_two: 42} }
+            summon name :: () -> string { [32, 42] }
+            summon name :: () -> string { [|32, 42|] }
+            summon name :: () -> string { Constructor }
+            summon name :: () -> string { Constructor([32]) }
+            summon name :: () -> string { Constructor([32], [32]) }
         }
         "#;
 
@@ -376,7 +379,7 @@ mod parse_tests {
     fn module_def_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 10000000 {
             use 9b397a7b41de899b986208961e34b09f52166315e1be0ac62e6aed2f854eb55b::{
                 fun,
                 Type,
@@ -432,12 +435,12 @@ mod parse_tests {
                 }
             }
             data Sheep =
-                | Version1 ({name: String, naked: Bool})
-                | Version2 ({name: String, naked: Bool, breed: Breed})
-            sketch data Sheep<I> = sketch Sheep({push: (I) -> Stack<I>, pop: ()->(I, Stack<I>), is_empty: Bool})
+                | Version1 ({name: string, naked: bool})
+                | Version2 ({name: string, naked: bool, breed: Breed})
+            sketch data Sheep<I> = sketch Sheep({push: (I) -> Stack<I>, pop: ()->(I, Stack<I>), is_empty: bool})
             sketch data Sheep =
-                | sketch Version1 ({name: String, naked: Bool})
-                | summon Version2 ({name: String, naked: Bool, breed: Breed})
+                | sketch Version1 ({name: string, naked: bool})
+                | summon Version2 ({name: string, naked: bool, breed: Breed})
             summon data DataType = sketch Constructor
             sketch data DataType = sketch Constructor
             stable data DataType = stable Constructor
@@ -449,7 +452,7 @@ mod parse_tests {
             summon name1 :: () -> Generic<Simple> {
                 hi = {1:2}
             }
-            sketch name2 :: () -> {I64: Simple} {
+            sketch name2 :: () -> {i64: Simple} {
                 hi = {1:2}
             }
             stable name3 :: () -> [Generic<Simple>] {
@@ -485,28 +488,9 @@ mod parse_tests {
 
     #[test]
     fn selective_receive_and_send_test() {
-        let input =
-        r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
-            summon name :: (str: String) -> String {
-                process ! "string"
-                receive all {
-                    | [||] => { skip }
-                    | [] => { skip }
-                    | true => { skip }
-                }
-                receive process {
-                    | false => { skip }
-                    | 'h' => { skip }
-                    | _ => { skip }
-                    | "hello" => { skip }
-                    | val: Int => { skip }
-                }
-            }
-        }
-        "#;
-
-        let parsed = SioParser::parse(Rule::main, &input);
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/selective_receive_and_send_test.sio").expect("cannot read file");
+        let parsed = SioParser::parse(Rule::main, &unparsed_file);
         match parsed {
             Ok(mut res) => {
                 for statement in res.next().unwrap().into_inner() {
@@ -529,7 +513,7 @@ mod parse_tests {
     fn generic_function_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             summon name :: () { skip }
             summon name :: (a: A) { skip }
             summon name<A> :: (a: A) -> A { skip }
@@ -561,15 +545,15 @@ mod parse_tests {
     fn process_spawn_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             summon name :: () {
                 let pid1: Pid
                 let pid2: Pid
-                spawn(79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server, loop, [])
-                pid1 = spawn(79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server, loop, [])
+                spawn(79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server, loop, [])
+                pid1 = spawn(79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server, loop, [])
                 pid2 = spawn(pid1, loop, [])
                 register("process_name/name", identity)
-                register(url_string, 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server)
+                register(url_string, 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server)
             }
         }
         "#;
@@ -597,7 +581,7 @@ mod parse_tests {
     fn thread_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             summon name :: () {
                 thread {
                     skip
@@ -629,7 +613,7 @@ mod parse_tests {
     fn lazy_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             summon lazy name :: () {
                 skip
             }
@@ -659,7 +643,7 @@ mod parse_tests {
     fn procedure_in_where_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
             name :: () {
                 skip
             } in {
@@ -693,8 +677,8 @@ mod parse_tests {
     fn higher_order_procedure_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
-            name :: () -> {String:Hi} {
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
+            name :: () -> {string:Hi} {
                 skip
             }
             name :: (a: ()->Hi) {
@@ -709,13 +693,13 @@ mod parse_tests {
             name :: (a: ([B])->[Hi], b: (C)->[|3;Hi|]) {
                 skip
             }
-            name<A> :: (a: (A, A)->(A)->Bool, b: (A, A)->(A)->Hi) -> Hi{
+            name<A> :: (a: (A, A)->(A)->bool, b: (A, A)->(A)->Hi) -> Hi{
                 skip
             }
             name<A,B,C> :: (
-                a: (A, A) -> (C) -> Bool,
-                b: (A, B) -> (C) -> Bool
-            ) -> (A)->Bool {
+                a: (A, A) -> (C) -> bool,
+                b: (A, B) -> (C) -> bool
+            ) -> (A)->bool {
                 skip
             }
         }
@@ -744,18 +728,18 @@ mod parse_tests {
     fn stack_test() {
         let input =
         r#"
-        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::src/area/server {
-            sketch data Stack<I> = sketch Stack({ push: (I)->Stack<I>, pop: ()->(I, Stack<I>), is_empty: Bool})
+        mod 79f708c25a23ed367610facc14035adc7ba4b1bfa9252ef55c6c24f1b9b03abd::/src/area/server 100 {
+            sketch data Stack<I> = sketch Stack({ push: (I)->Stack<I>, pop: ()->(I, Stack<I>), is_empty: bool})
             stack<I> :: (inner_stack: [I]) -> Stack<I> {
                 push :: (item: I) -> Stack<I> {
-                    stack(item|stack)
+                    stack([item|stack])
                 }
                 pop :: () -> (I, Stack<I>) {
                     match inner_stack {
                         | [head|tail] => {(head, stack(tail))}
                     }
                 }
-                is_empty :: () -> Bool {
+                is_empty :: () -> bool {
                     inner_stack == []
                 }
             } in {
@@ -791,6 +775,30 @@ mod parse_tests {
         use std::fs;
         let unparsed_file = fs::read_to_string("examples/area_server.sio").expect("cannot read file");
         let parsed = SioParser::parse(Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn collection_operations_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/collection_operations.sio").expect("cannot read file");
+        let parsed = SioParser::parse(Rule::main, &unparsed_file);
+        println!("{:#?}", parsed);
         match parsed {
             Ok(mut res) => {
                 for statement in res.next().unwrap().into_inner() {
