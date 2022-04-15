0b76158618e6c8cdafe5c41f29602505bb0823fc
Stewart Mackenzie <setori88@gmail.com>
2022-04-15 15:23:00 +0800

    Problem: hex and bin primitives not present
    Solution: add them, because they are so useful to the programmer

---
+++ syntax/src/lib.rs
@@ -0,0 +1,480 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+pub use {
+    //syntax::{Span, SrcId},
+    //chumsky::{Parser},
+    pest::Parser,
+};
+
+pub mod sio {
+    #[derive(Parser)]
+    //#[grammar = "grammar/csv.pest"]
+    #[grammar = "grammar/sio.pest"]
+    pub struct SioParser;
+}
+
+#[cfg(test)]
+mod parse_tests {
+    use super::*;
+
+    #[test]
+    fn data_variant_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/data_variant_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn pattern_match_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/pattern_match_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn capitalized_record_key_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/capitalized_record_key_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn var_to_var_binding_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/var_to_var_binding_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn print_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/print_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn return_value_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/return_value_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn module_def_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/module_def_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::variable_creation => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn selective_receive_and_send_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/selective_receive_and_send_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn generic_procedure_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/generic_procedure_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn process_spawn_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/process_spawn_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn thread_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/thread_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn lazy_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/lazy_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn procedure_in_where_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/procedure_in_where_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn higher_order_procedure_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/higher_order_procedure_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn stack_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/stack_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn area_server_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/area_server.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn collection_operations_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/collection_operations.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn ffi_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/ffi.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+
+    #[test]
+    fn link_monitor_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/link_monitor.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+    #[test]
+    fn bin_hex_test() {
+        use std::fs;
+        let unparsed_file = fs::read_to_string("examples/binary_hexadecimal_test.sio").expect("cannot read file");
+        let parsed = sio::SioParser::parse(sio::Rule::main, &unparsed_file);
+        println!("{:#?}", parsed);
+        match parsed {
+            Ok(mut res) => {
+                for statement in res.next().unwrap().into_inner() {
+                    match statement.as_rule() {
+                        sio::Rule::module_def => {
+                            println!("{:#?}", statement);
+                        }
+                        _ => (),
+                    }
+                }
+            },
+            Err(e) => {
+                println!("{:#?}", e);
+                panic!()
+            }
+        }
+    }
+}
