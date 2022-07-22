2671166df0f6a28286fec8fc398910cd9e679993
Stewart Mackenzie <setori88@gmail.com>
2022-07-22 15:51:17 +0800

    Problem: rp target not available
    Solution: add it

--- examples/rp/src/bin/blinky.rs
+++ examples/rp/src/bin/blinky.rs
@@ -5,24 +5,24 @@
 use defmt::*;
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_stm32::Peripherals;
+use embassy_rp::{gpio, Peripherals};
+use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 use sio_vm::{tick};
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
-    let mut led = Output::new(p.PB5, Level::High, Speed::Low);
+    let mut led = Output::new(p.PIN_25, Level::Low);
 
     loop {
         info!("{}", tick().await);
 
         info!("led on!");
         led.set_high();
-        Timer::after(Duration::from_millis(300)).await;
+        Timer::after(Duration::from_secs(1)).await;
 
         info!("led off!");
         led.set_low();
-        Timer::after(Duration::from_millis(300)).await;
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
